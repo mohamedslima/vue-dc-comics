d31eb9dd8f4fedac9ccf5b19258b3b0f9ba9bc78
mohamedslima <hammaslima@gmail.com>
2022-05-17 16:51:29 +0200

    header completed and started footer

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,13 +3,79 @@
     <div class="header__logo">
       <img src="../assets/img/dc-logo.png" alt="" />
     </div>
-    <div class="navigation__bar"></div>
+    <div class="header__menu">
+      <ul>
+        <li
+          v-for="(item, index) in links"
+          :key="index"
+          :class="{ hover: item.active }"
+        >
+          <a href="#"> {{ item.text }}</a>
+        </li>
+      </ul>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
   name: "AppHeader",
+  data: function () {
+    return {
+      links: [
+        {
+          url: "#",
+          text: "CHARACTERS",
+          active: false,
+        },
+        {
+          url: "#",
+          text: "COMICS",
+          active: true,
+        },
+        {
+          url: "#",
+          text: "MOVIES",
+          active: false,
+        },
+        {
+          url: "#",
+          text: "TV",
+          active: false,
+        },
+        {
+          url: "#",
+          text: "GAMES",
+          active: false,
+        },
+        {
+          url: "#",
+          text: "COLLECTIBLES",
+          active: false,
+        },
+        {
+          url: "#",
+          text: "VIDEOS",
+          active: false,
+        },
+        {
+          url: "#",
+          text: "FANS",
+          active: false,
+        },
+        {
+          url: "#",
+          text: "NEWS",
+          active: false,
+        },
+        {
+          url: "#",
+          text: "SHOP",
+          active: false,
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -19,10 +85,31 @@ export default {
 .header {
   width: $general-width-page;
   margin: $general-center-page;
-  background-color: red;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   &__logo {
     img {
-      width: 8%;
+      width: 70%;
+    }
+  }
+  &__menu {
+    ul {
+      display: flex;
+      li {
+        list-style: none;
+        margin-left: 0.8rem;
+
+        &.hover {
+          color: $menu-color-active;
+          border-bottom: $menu-border-active;
+        }
+        a {
+          text-decoration: $leave-decoration-text;
+          color: black;
+          line-height: 5px;
+        }
+      }
     }
   }
 }
